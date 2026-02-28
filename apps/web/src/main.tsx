ace40c1a7127b798625f5dcd56b7536cde1e09f1
Eli Parker <eliparkdev@icloud.com>
2026-02-28 01:31:52 -0700

    Added board page concept

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-// import BoardPage from './pages/BoardPage.tsx'
+import BoardPage from './Board.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="/board" element={<BoardPage />} /> */}
+        <Route path="/board" element={<BoardPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
